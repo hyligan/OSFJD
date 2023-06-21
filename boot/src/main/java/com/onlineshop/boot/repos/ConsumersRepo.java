68e3e5e1369b06514360bddb66b37bb238deb814
petroz <petroz@ext.inditex.com>
2023-06-21 20:48:38 +0300

    some repos

--- boot/src/main/java/com/onlineshop/boot/repos/ConsumersRepo.java
+++ boot/src/main/java/com/onlineshop/boot/repos/ConsumersRepo.java
@@ -2,7 +2,11 @@ package com.onlineshop.boot.repos;
 
 
 import com.onlineshop.boot.entities.ConsumersEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ConsumersRepo extends CrudRepository<ConsumersEntity, Long> {
+    @Query("select p from ConsumersEntity join AddressEntity" +
+            " join ")
+    ConsumersEntity getByConsumerId(Long id);
 }
